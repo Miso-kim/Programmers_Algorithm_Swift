20eb1809db0456197802e7e2c1092747286d596f
김미소 <misokeem@finnq.com>
2022-03-23 18:00:02 +0900

    프로그래머스 해시 알고리즘 위장 풀이

---
+++ Programmers_Algorithm_Swift/Programmers_Algorithm_Swift/programmers/hash_spy.swift
@@ -0,0 +1,35 @@
+//
+//  hash_spy.swift
+//  Programmers_Algorithm_Swift
+//
+//  Created by misoKim on 2022/03/23.
+//
+
+import Foundation
+class Spy {
+    func solution(_ clothes:[[String]]) -> Int {
+        var clothesDict : [String: Int] = [:]
+        for cloth in clothes {
+            let key = cloth[1]
+            if !clothesDict.keys.contains(key) {
+                clothesDict[key] = 0
+            }
+            clothesDict[key]? += 1
+        }
+        
+        var count = 1
+        for num in clothesDict.values {
+            count *= (num + 1)
+        }
+        return count-1
+    }
+
+    func solution2(_ clothes:[[String]]) -> Int {
+        let types = clothes.compactMap({ $0.last })
+        let typeSet = Set(types)
+        let categories = Array(typeSet)
+        let counts = categories.map({ category in
+            return clothes.filter({ $0.last == category }).count + 1 })
+        return counts.reduce(1, { $0 * $1 }) - 1
+    }
+}
